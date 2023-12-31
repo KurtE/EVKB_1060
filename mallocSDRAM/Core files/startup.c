a879721d66117ca5f489cc916f55518d8f356a5a
Mike S <CyberMerln@gmail.com>
2023-12-31 17:31:15 -0500

    Updated startup.c and pin spreadsheet

--- mallocSDRAM/Core files/startup.c
+++ mallocSDRAM/Core files/startup.c
@@ -346,11 +346,14 @@ FLASHMEM void configure_cache(void)
 
 	SCB_MPU_RBAR = 0x70000000 | REGION(i++); // FlexSPI2
 	SCB_MPU_RASR = MEM_CACHE_WBWA | READWRITE | NOEXEC | SIZE_16M;
-#ifdef extSDRAM
+//#ifdef extSDRAM
 	SCB_MPU_RBAR = 0x80000000 | REGION(i++); // SEMC: SDRAM, NAND, SRAM, etc
 	SCB_MPU_RASR = MEM_CACHE_WBWA | READWRITE | NOEXEC | SIZE_1G;
 	// SDRAM PCB: https://forum.pjrc.com/index.php?threads/73898/#post-334041
-#endif
+	SCB_MPU_RBAR = 0x81E00000 | REGION(i++); // SEMC: SDRAM, NAND, SRAM, etc
+	SCB_MPU_RASR = MEM_NOCACHE | READWRITE | NOEXEC | SIZE_2M;
+    
+//#endif
 	SCB_MPU_CTRL = SCB_MPU_CTRL_ENABLE;
 
 	// cache enable, ARM DDI0403E, pg 628
